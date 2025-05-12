76a622f0edd1a1ee237480d7b900a27f34fc2d54
Franco Stival <166665793+FrancoStival@users.noreply.github.com>
2025-05-12 15:26:00 +0200

    Initial Commit
    Added the project from the Exam branch of the repository of Nacho

---
+++ HobbyManiaManager/Forms/MainForm.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using HobbyManiaManager.Models;
+using HobbyManiaManager.Forms;
+using System.Drawing;
+using HobbyManiaManager.Utils;
+using System.Linq;
+
+namespace HobbyManiaManager
+{
+    public partial class MainForm : Form
+    {
+        private readonly MoviesRepository _moviesRepository;
+        private readonly CustomersRepository _customersRepository;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            _moviesRepository = MoviesRepository.Instance;
+            _customersRepository = CustomersRepository.Instance;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            this.AutoScaleMode = AutoScaleMode.None;
+
+            LoadMovies();
+            CreateRandomCustomers();
+            BuildTabs();
+
+            labelMoviesCounter.Text = $"{_moviesRepository.Count} movies loaded.";
+        }
+
+        private void BuildTabs()
+        {
+            tabControlMain.Font = new Font("Segoe UI", 16, FontStyle.Regular);
+
+            var catalog = new CatalogForm();
+            ConfigureForm(catalog);
+            tabPageCatalog.Controls.Add(catalog);
+            catalog.Show();
+
+            var customers = new ListCustomersForm();
+            ConfigureForm(customers);
+            tabPageCustomers.Controls.Add(customers);
+        }
+
+        private void ConfigureForm(Form form)
+        {
+            form.TopLevel = false;
+            form.FormBorderStyle = FormBorderStyle.None;
+            form.AutoScaleMode = AutoScaleMode.None;
+            form.Visible = true;
+            form.Font = Font;
+            form.Location = new Point(0, 0);
+        }
+
+        private void LoadMovies()
+        {
+            try
+            {
+                string filePath = "Resources/tmdb_movies.json";
+                string json = File.ReadAllText(filePath);
+                var movies = JsonConvert.DeserializeObject<List<Movie>>(json);
+                if (movies != null)
+                {
+                    _moviesRepository.AddAll(movies.Where(m => m.Status.ToLower().Equals("released")).ToList());
+                }
+                else
+                {
+                    MessageBox.Show("No movies data found or the file is corrupted.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred while loading movies: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void CreateRandomCustomers()
+        {
+            var generator = new RandomCustomerGenerator();
+            for (int i = 0; i < 100; i++) {
+                generator.Generate();
+                var c = new Customer(Customer.NextCustomerId, generator.AvatarUrl, generator.FullName, generator.Email, generator.PhoneNumber, generator.RegistrationDate);
+                _customersRepository.Add(c);
+            }
+        }
+
+        private void buttonCatalog_Click(object sender, EventArgs e)
+        {
+            var catalogForm = new CatalogForm();
+            catalogForm.ShowDialog();
+        }
+
+        private void buttonCreateCustomer_Click(object sender, EventArgs e)
+        {
+            var createCustomer = new EditCustomerForm(this);
+            createCustomer.ShowDialog();
+        }
+
+        private void buttonUpdateCustomer_Click(object sender, EventArgs e)
+        {
+            var c = _customersRepository.GetById(1);
+            var form = new EditCustomerForm(this, c);
+            form.ShowDialog();
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ HobbyManiaManager/Models/Movie.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace HobbyManiaManager.Models
+{
+    public class Movie : ICloneable
+    {
+
+        private const string BaseImageUrl = "https://image.tmdb.org/t/p/w342";
+
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Director { get; set; }
+
+        [JsonProperty("original_title")]
+        public string OriginalTitle { get; set; }
+
+        [JsonProperty("release_date")]
+        public DateTime ReleaseDate { get; set; }
+
+        public string Status { get; set; }
+
+        public string Overview { get; set; }
+
+        [JsonProperty("poster_path")]
+        public string PosterPath { get; set; }
+
+        public string PosterUrl(int with = 342) => string.IsNullOrEmpty(this.PosterPath) ? null : $"https://image.tmdb.org/t/p/w{with}{PosterPath}";
+
+        [JsonProperty("backdrop_path")]
+        public string BackdropPath { get; set; }
+
+        public string BackdropUrl => string.IsNullOrEmpty(this.BackdropPath) ? null : $"{BaseImageUrl}{BackdropPath}";
+
+        [JsonProperty("vote_average")]
+        public double VoteAverage { get; set; }
+
+        [JsonProperty("vote_count")]
+        public int VoteCount { get; set; }
+
+        [JsonProperty("genres")]
+        public List<Genre> Genres { get; set; }
+
+        public string GenresAsSting => string.Join(", ", Genres.Select(g => g.Name));
+
+        public object Clone()
+        {
+            return new Movie
+            {
+                Id = this.Id,
+                Title = this.Title,
+                Director = this.Director,
+                OriginalTitle = this.OriginalTitle,
+                ReleaseDate = this.ReleaseDate,
+                Status = this.Status,
+                Overview = this.Overview,
+                PosterPath = this.PosterPath,
+                BackdropPath = this.BackdropPath,
+                VoteAverage = this.VoteAverage,
+                VoteCount = this.VoteCount,
+                Genres = this.Genres?.Select(g => new Genre { Id = g.Id, Name = g.Name }).ToList()
+             };
+        }
+
+        public class Genre
+        {
+            [JsonProperty("id")]
+            public int Id { get; set; }
+
+            [JsonProperty("name")]
+            public string Name { get; set; }
+        }
+    }
+}
